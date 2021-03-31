5545dd8f3ed5f844ca0a818d5a8ac96f98b63505
Alexander Tsvetaev <alex_tsv@bk.ru>
2021-03-31 22:30:15 +0600

    some changes in Dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,17 +1,24 @@
+import { NavLink } from 'react-router-dom'
 import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
-                <div className={s.dialog}>Alex</div>
-                <div className={s.dialog}>222222</div>
-                <div className={s.dialog}>33333333</div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/1'>Alex</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialog/2'>Aleksaaaa</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialog/3'>Alexey</NavLink>
+                </div>
             </div>
             <div className={s.messages}>
-                <div className={s.message}>123</div>
-                <div className={s.message}>445655</div>
-                <div className={s.message}>954894</div>
+                <div className={s.message}>Message 1111</div>
+                <div className={s.message}>Message 2222</div>
+                <div className={s.message}>Message 3333</div>
             </div>
         </div>
     )
