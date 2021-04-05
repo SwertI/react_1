f2d99f2e6e393cbf04bdfe121f60de04b91ba4f6
Alexander Tsvetaev <alex_tsv@bk.ru>
2021-04-05 08:26:39 +0600

    react lesson 24

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -15,17 +15,27 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+    let dialogsData = [
+        {id: 1, name:'Alex'},
+        {id: 2, name: 'Aleksaaaa'},
+        {id: 3, name: 'Alexey'}
+    ]
+    let messageData = [
+        {id: 1, message: 'HI'},
+        {id: 2, message: 'Message 222'},
+        {id: 3, message: 'Message 333'},
+    ]
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
-                <DialogItem name='Alex' id='1' />
-                <DialogItem name='Aleksaaaa' id='2' />
-                <DialogItem name='Alexey' id='3' />
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
             </div>
             <div className={s.messages}>
-                <Message message='Hi' />
-                <Message message='Message 222' />
-                <Message message='Message 333' />
+                <Message message={messageData[0].message} />
+                <Message message={messageData[1].message} />
+                <Message message={messageData[2].message} />
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,9 +2,14 @@ import s from './MyPosts.module.css'
 import Post from './Post/Post'
 
 const MyPosts = () => {
+    let posteData = [
+        { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+        { id: 2, message: "It's my first post", likesCount: 11 }
+    ]
+
     return (
         <div className={s.postsBlock}>
-            <h3>My posts</h3> 
+            <h3>My posts</h3>
             <div>
                 <div>
                     <textarea name='' id='' cols='166' rows='5'></textarea>
@@ -16,8 +21,8 @@ const MyPosts = () => {
             </div>
             <div>New Posts</div>
             <div className={s.posts}>
-                <Post message='Hi, how are you?' likesCounts='20' />
-                <Post message="It's my first post" likesCounts='10' />
+                <Post message={posteData[0].message} likesCounts={posteData[0].likesCount} />
+                <Post message={posteData[1].message} likesCounts={posteData[1].likesCount} />
             </div>
         </div>
     )
